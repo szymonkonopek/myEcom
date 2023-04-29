eada181c01313d2af621c0e186764209cd921564
szymonkonopek <sk.gornik@gmail.com>
2023-04-29 21:37:29 +0200

    feat: bootstrap

--- src/main/java/pl/konopek/productcatalog/controllers/ProductCatalogController.java
+++ src/main/java/pl/konopek/productcatalog/controllers/ProductCatalogController.java
@@ -1,7 +1,9 @@
-package pl.konopek.productcatalog;
+package pl.konopek.productcatalog.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.konopek.productcatalog.Product;
+import pl.konopek.productcatalog.ProductCatalog;
 
 import java.util.Collections;
 import java.util.List;
